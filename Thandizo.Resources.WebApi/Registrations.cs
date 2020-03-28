a2512bdbd3d837dd024aea130dd982dfee96e69e
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-03-28 13:49:22 +0200

    added Registrations for registering service classes

---
+++ Thandizo.Resources.WebApi/Registrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Thandizo.FacilityResources.BLL.Services;
+
+namespace Thandizo.Resources.WebApi
+{
+    public static class Registrations
+    {
+        /// <summary>
+        /// Registers domain services to the specified
+        /// service descriptor
+        /// </summary>
+        /// <param name="services"></param>
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IResourceAllocationRepository, ResourceAllocationRepository>();
+            return services.AddScoped<IResourceService, ResourceService>();
+        }
+    }
+}
