f526e66cc30dc40734b09b4496d69fe990c9d899
Vitumbiko Vinkhumbo <vitumbiko.vinkhumbo@angledimension.com>
2020-03-30 18:19:43 +0200

    Added ResourcesAllocationController class

---
+++ Thandizo.Resources.WebApi/Controllers/ResourcesAllocationController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Thandizo.ApiExtensions.Filters;
+using Thandizo.ApiExtensions.General;
+using Thandizo.DataModels.Resources;
+using Thandizo.FacilityResources.BLL.Services;
+
+namespace Thandizo.Core.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesAllocationController : ControllerBase
+    {
+        IResourceAllocationService _service;
+
+        public ResourcesAllocationController(IResourceAllocationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetById")]
+        [CatchException(MessageHelper.GetItemError)]
+        public async Task<IActionResult> GetById([FromQuery] int resourceAllocationId)
+        {
+            var response = await _service.Get(resourceAllocationId);
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpGet("GetAll")]
+        [CatchException(MessageHelper.GetListError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _service.Get();
+
+            if (response.IsErrorOccured)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Result);
+        }
+
+        [HttpPost("Add")]
+        [CatchException(MessageHelper.AddNewError)]
+        public async Task<IActionResult> Add([FromBody]ResourceAllocationDTO resourceAllocation)
+        {
+            var outputHandler = await _service.Add(resourceAllocation);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Created("", outputHandler);
+        }
+
+        [HttpPut("Update")]
+        [CatchException(MessageHelper.UpdateError)]
+        public async Task<IActionResult> Update([FromBody]ResourceAllocationDTO resourceAllocation)
+        {
+            var outputHandler = await _service.Update(resourceAllocation);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+
+        [HttpDelete("Delete")]
+        [CatchException(MessageHelper.DeleteError)]
+        public async Task<IActionResult> Delete([FromQuery]int resourceAllocationId)
+        {
+            var outputHandler = await _service.Delete(resourceAllocationId);
+            if (outputHandler.IsErrorOccured)
+            {
+                return BadRequest(outputHandler);
+            }
+
+            return Ok(outputHandler);
+        }
+    }
+}
