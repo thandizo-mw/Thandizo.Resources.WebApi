873614fb83ea01262a46d0484082d2fe8574766a
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-04-01 09:55:23 +0200

    changed to return string instead of an object for ADD and UPDATE actions in all controllers

--- Thandizo.Resources.WebApi/Controllers/ResourcesAllocationController.cs
+++ Thandizo.Resources.WebApi/Controllers/ResourcesAllocationController.cs
@@ -53,10 +53,10 @@ namespace Thandizo.Core.WebApi.Controllers
             var outputHandler = await _service.Add(resourceAllocation);
             if (outputHandler.IsErrorOccured)
             {
-                return BadRequest(outputHandler);
+                return BadRequest(outputHandler.Message);
             }
 
-            return Created("", outputHandler);
+            return Created("", outputHandler.Message);
         }
 
         [HttpPut("Update")]
@@ -66,10 +66,10 @@ namespace Thandizo.Core.WebApi.Controllers
             var outputHandler = await _service.Update(resourceAllocation);
             if (outputHandler.IsErrorOccured)
             {
-                return BadRequest(outputHandler);
+                return BadRequest(outputHandler.Message);
             }
 
-            return Ok(outputHandler);
+            return Ok(outputHandler.Message);
         }
 
         [HttpDelete("Delete")]
@@ -79,10 +79,10 @@ namespace Thandizo.Core.WebApi.Controllers
             var outputHandler = await _service.Delete(resourceAllocationId);
             if (outputHandler.IsErrorOccured)
             {
-                return BadRequest(outputHandler);
+                return BadRequest(outputHandler.Message);
             }
 
-            return Ok(outputHandler);
+            return Ok(outputHandler.Message);
         }
     }
 }
--- Thandizo.Resources.WebApi/Controllers/ResourcesController.cs
+++ Thandizo.Resources.WebApi/Controllers/ResourcesController.cs
@@ -53,10 +53,10 @@ namespace Thandizo.Core.WebApi.Controllers
             var outputHandler = await _service.Add(resource);
             if (outputHandler.IsErrorOccured)
             {
-                return BadRequest(outputHandler);
+                return BadRequest(outputHandler.Message);
             }
 
-            return Created("", outputHandler);
+            return Created("", outputHandler.Message);
         }
 
         [HttpPut("Update")]
@@ -66,10 +66,10 @@ namespace Thandizo.Core.WebApi.Controllers
             var outputHandler = await _service.Update(resource);
             if (outputHandler.IsErrorOccured)
             {
-                return BadRequest(outputHandler);
+                return BadRequest(outputHandler.Message);
             }
 
-            return Ok(outputHandler);
+            return Ok(outputHandler.Message);
         }
 
         [HttpDelete("Delete")]
@@ -79,10 +79,10 @@ namespace Thandizo.Core.WebApi.Controllers
             var outputHandler = await _service.Delete(resourceId);
             if (outputHandler.IsErrorOccured)
             {
-                return BadRequest(outputHandler);
+                return BadRequest(outputHandler.Message);
             }
 
-            return Ok(outputHandler);
+            return Ok(outputHandler.Message);
         }
     }
 }
